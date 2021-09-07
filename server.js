aa99afa51d60f428550f98c3dbe69fa249dfc328
Rashid S Chowdhury <rshdschwdhry@gmail.com>
2021-09-07 23:57:30 +0800

    Daily commit goal. Issues with frontend and database section.

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+const Document = require("./models/Document");
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/wastebin", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
 
 app.get("/", (req, res) => {
   const code = `
@@ -12,11 +21,31 @@ app.get("/", (req, res) => {
     to create a new file to share with others.
     `;
 
-  res.render("code-display", { code });
+  res.render("code-display", { code, language: "plaintext" });
 });
 
 app.get("/new", (req, res) => {
   res.render("new");
+});
+
+app.post("/save", async (req, res) => {
+  const value = req.body.value;
+  try {
+    const document = await Document.create({ value });
+    res.redirect(`/${document.id}`);
+  } catch (e) {
+    res.render("new", { value });
+  }
+});
+
+app.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const document = await Document.findById(id);
+    res.render("code-display", { code: document.value });
+  } catch (e) {
+    res.redirect("/");
+  }
 });
 
 app.listen(3000);
